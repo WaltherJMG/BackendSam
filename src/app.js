55bcefd54f74df4f35d473c1b8ca27f0eaf7c400
WaltherJMG <wmatute5454@utm.edu.ec>
2026-01-10 21:44:04 -0500

    first commit

--- src/app.js
+++ src/app.js
@@ -11,7 +11,8 @@ const app = express();
 const port = process.env.PORT || 3000
 
 const withelist = [
-    'http://localhost:5173'
+    'http://localhost:5173',
+    'https://frotend-sam.vercel.app'
 ]
 
 app.use(cors({
